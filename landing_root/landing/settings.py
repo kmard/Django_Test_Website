c6050acd5bab8ecef60c8fdae2ad0714c7509a01
KM <tsnok777@gmail.com>
2022-06-07 19:28:27 +0300

    first page

--- landing_root/landing/settings.py
+++ landing_root/landing/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'landing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
