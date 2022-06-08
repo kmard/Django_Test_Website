e80ed88c44b4dec9ffdc229bebff3b595facca50
KM <tsnok777@gmail.com>
2022-06-08 16:14:46 +0300

    create crm

--- landing_root/landing/settings.py
+++ landing_root/landing/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# 'crm.apps.CrmConfigdjango'
 INSTALLED_APPS = [
+    'crm.apps.CrmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
