82dd1fc89984182ffb543487bdf0af20dadd3a59
KM <tsnok777@gmail.com>
2022-06-26 20:00:50 +0300

    setting Css adminka

--- landing_root/cms/admin.py
+++ landing_root/cms/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import CmsSlider
 
 
+class CmsAdmin(admin.ModelAdmin):
+    list_display = ('cms_title','cms_text','cms_css','get_image')
+    list_display_links = ('cms_title',)
+    list_editable = ('cms_css',)
+    fields = ('cms_title','cms_text','cms_css','get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self,obj):
+        if obj.cms_img:
+            return mark_safe(f'<img src="{obj.cms_img.url}" width="80px"')
+        else:
+            print('element has not image'+str(obj))
+
+    get_image.short_description = 'image'
+
+
+
+
 # Register your models here.
-admin.site.register(CmsSlider)
+admin.site.register(CmsSlider,CmsAdmin)
